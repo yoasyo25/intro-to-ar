a5df9596aa4629fe707dad49f304e0ffb9b2f338
Sal Espinosa <sespinos@gmail.com>
2017-01-26 23:04:44 -0700

    Create additional tests

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,7 +1,9 @@
 class Horse < ActiveRecord::Base
   belongs_to :jockey
   belongs_to :breed
-  validates :name, presence: true
+  validates :name,           presence: true, uniqueness: true
+  validates :age,            presence: true
+  validates :total_winnings, presence: true
 
   def self.total_winnings
     sum(:total_winnings)
--- spec/models/horse_spec.rb
+++ spec/models/horse_spec.rb
@@ -8,17 +8,55 @@ describe Horse do
 
       expect(Horse.total_winnings).to eq(7)
     end
+
+    it 'returns total winnings for the horses of a single jockey' do
+      jockey_1 = Jockey.create(name: "Chava")
+      jockey_2 = Jockey.create(name: "Penelope")
+      jockey_1.horses.create(name: "Chuy", age: 22, total_winnings: 3)
+      jockey_1.horses.create(name: "Chema", age: 24, total_winnings: 4)
+      jockey_2.horses.create(name: "Pecos", age: 25, total_winnings: 8)
+
+      expect(jockey_1.horses.total_winnings).to eq(7)
+    end
+  end
+
+  describe '.average_winnings' do
+    it "returns the average winnings for all Horses" do
+      Horse.create(name: "Chuy", age: 22, total_winnings: 3)
+      Horse.create(name: "Chindo", age: 24, total_winnings: 5)
+
+      expect(Horse.average_winnings).to eq(4)
+    end
   end
 
   describe 'validations' do
     it 'is valid with valid attributes' do
-      horse = Horse.new(name: "George")
+      horse = Horse.new(name: "George", age: 22, total_winnings: 3)
 
       expect(horse).to be_valid
     end
 
     it 'validates presence of name' do
       horse = Horse.new(age: 22, total_winnings: 4)
+
+      expect(horse).to_not be_valid
+    end
+
+    it 'validates presence of age' do
+      horse = Horse.new(name: "Penelope", total_winnings: 4)
+
+      expect(horse).to_not be_valid
+    end
+
+    it 'validates presence of age' do
+      horse = Horse.new(name: "Penelope", age: 22)
+
+      expect(horse).to_not be_valid
+    end
+
+    it 'validates uniqueness of name' do
+      Horse.create(name: "Chuy", age: 22, total_winnings: 3)
+      horse = Horse.create(name: "Chuy", age: 44, total_winnings: 4)
 
       expect(horse).to_not be_valid
     end
